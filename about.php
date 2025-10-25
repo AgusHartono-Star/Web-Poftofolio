56c1eae15fc85688f3b466a8ae88312910c2bc4a
AgusHartono-Star <agushartonobusiness@gmail.com>
2025-10-25 20:19:30 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,171 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>My Web Portofolio</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap Icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+        <!-- SimpleLightbox plugin CSS-->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+     <body id="page-top">
+              <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#page-top">Home Page</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto my-2 my-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="index.php#portfolio">Portfolio</a></li>
+                        <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+            <!-- bodytop -->
+        <!-- Footer-->
+        <!-- <footer class="bg-light py-5">
+            <div class="container px-4 px-lg-5"><div class="small text-center text-muted">Copyright &copy; 2023 - Company Name</div></div>
+        </footer> -->
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- SimpleLightbox plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
+       <!-- About-->
+             <section class="page-section bg-primary" id="about">
+            <div class="container px-2 px-lg-3">
+                  </section>
+         <div id="about">
+            <div class="container-fluid p-0">
+                <div class="row g-0">
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/IMG-20251025-WA0013.jpg" title="Project Name">
+                             <img src="assets/img/portfolio/fullsize/IMG-20251025-WA0013.jpg" alt="Leadership Programs" class="img-fluid">
+                            <div class="portfolio-box-caption">
+                                <!-- <div class="project-category text-white-50">Category</div>
+                                <div class="project-name">Project Name</div> -->
+                            </div>
+                        </a>
+                    </div>
+                    
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/WhatsApp Image 2025-10-25 at 14.47.31_7a9c1679.jpg" title="Project Name">
+                            <img src="assets/img/portfolio/fullsize/WhatsApp Image 2025-10-25 at 14.47.31_7a9c1679.jpg" alt="Leadership Programs" class="img-fluid">
+                            <div class="portfolio-box-caption">
+                                <!-- <div class="project-category text-white-50">Category</div>
+                                <div class="project-name">Project Name</div> -->
+                            </div>
+                        </a>
+                    </div>
+                    
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/IMG-20251025-WA0014.jpg" title="Project Name">
+                           <img src="assets/img/portfolio/fullsize/IMG-20251025-WA0014.jpg" alt="Leadership Programs" class="img-fluid">
+                            <div class="portfolio-box-caption">
+                                <!-- <div class="Claro-Hotel text-white-50">Claro Hotel</div>
+                                <div class="project-name">Agus Hartono</div> -->
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+ 
+                <!-- <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8 text-center">
+                                <div class="highlights-section"> --> 
+          <!-- <div class="row g-4">
+            <div class="col-lg-4">
+              <div class="highlight-card">
+                <div class="highlight-image">
+                    <div class="highlight-content"> -->
+                 
+                  
+                  
+                </div>
+                        <!-- <h2 class="text-white mt-0">We've got what you need!</h2>
+                        <hr class="divider divider-light" />
+                        C:\xampp\htdocs\My_Website\assets\img\portfolio\fullsize\WhatsApp Image 2025-10-25 at 14.47.30_dbbc60f3.jpg
+                        <p class="text-white-75 mb-4">Start Bootstrap has everything you need to get your new website up and running in no time! Choose one of our open source, free to download, and easy to use themes! No strings attached!</p>
+                        <a class="btn btn-light btn-xl" href="#services">Get Started!</a> -->
+                    </div>
+                </div>
+            </div>
+      
+        <!-- Services-->
+        <section class="page-section" id="services">
+            <div class="container px-4 px-lg-5">
+                <h2 class="text-center mt-0">Social Media</h2>
+                <hr class="divider" />
+                <div class="row gx-4 gx-lg-5">
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <!-- <div class="mb-2"><i class="bi-gem fs-1 text-primary"></i></div> -->
+                             <img class="img-fluid" src="assets/img/portfolio/fullsize/youtube-logo.png"  width="100px" height="400px" alt="..." />
+                            <h3 class="h4 mb-2">Youtube</h3>
+                            <p class="text-muted mb-0">@Agus Hartono</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <img class="img-fluid" src="assets/img/portfolio/fullsize/instagram).jpeg"  width="100px" height="400px" alt="..." />
+                            <!-- <div class="mb-2"><i class="bi-laptop fs-1 text-primary"></i></div>  -->
+                            <h3 class="h4 mb-2"> Instagram </h3>
+                            <p class="text-muted mb-0">.@agushartono7337</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <!-- <div class="mb-2"><i class="bi-globe fs-1 text-primary"></i></div> -->
+                            <img class="img-fluid" src="assets/img/portfolio/fullsize/facebook-logo.jpg" width="100px" height="400px" alt="..." />
+                            <h3 class="h4 mb-2">Facebook</h3>
+                            <p class="text-muted mb-0">@Agus Kun </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                                                      <img class="img-fluid" src="assets/img/portfolio/fullsize/linkedin-logo.jpg" width="100px" height="400px" alt="..." />
+                            <!-- <div class="mb-2"><i class="bi-heart fs-1 text-primary"></i></div> -->
+                            <h3 class="h4 mb-2">Linkedin</h3>
+                            <p class="text-muted mb-0">@Agus Hartono</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+      </body>
+              <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- SimpleLightbox plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$host="localhost";
+$user="root";
+$password="";
+$db="dbdesa";
+$connect=mysqli_connect($localhost,$user,$user,$db)
+;
+?>
